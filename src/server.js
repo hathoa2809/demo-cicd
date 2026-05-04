279e8f1f5d1e2254db9f767f82b56bc8a28beae7
thoaha <hathoa2809@gmail.com>
2026-05-05 06:22:20 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -12,6 +12,8 @@ process.on('unhandledRejection', (err) => {
     console.log('UNHANDLED REJECTION! Shutting down...');
     console.log(err.name, err.message);
     server.close(() => {
-        process.exit(1);
+        if (process.env.NODE_ENV !== 'production') {
+            process.exit(1);
+        }
     });
 });
